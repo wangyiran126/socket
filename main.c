1db3339df3ae6bfae64e89f80a4826df1ef7ec90
wangyiran126 <i5v2y6c3>
2016-12-17 16:52:28 +0800

    socket

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 int main() {
     int  socketfd = socket(AF_UNIX,SOCK_STREAM,0);
-    struct sockaddr_in6 *sockaddrIn6;
-    void * socket = malloc(sizeof(struct sockaddr_in6));
-    if (socket != NULL){
-        sockaddrIn6 = (struct *sockaddr_in6)socket;
-    }
-    sockaddr.sa_family = AF_INET6;
-    bind(socketfd,&sockaddr, (socklen_t)sizeof(sockaddr));
+    socketfd++;
+    printf("%d",14);
+//    struct sockaddr_in6 * socket = malloc(sizeof(struct sockaddr_in6));
+//    socket->sin6_family
+//    socket.sa_family = AF_INET6;
+//    bind(socketfd,&sockaddr, (socklen_t)sizeof(sockaddr));
     return 0;
 }
